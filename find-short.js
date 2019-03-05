cfcd86a0f8f75ad347c7c90952b33bc4395582c5
Caleb Spain <44350280+calebspain@users.noreply.github.com>
2019-03-04 21:35:34 -0600

    Create find-short.js

---
+++ find-short.js
@@ -0,0 +1,14 @@
+
+// Find The Length Of The Shortest Word
+
+let str = 'Ripple ProofOfStake Dogecoin Steem ProofOfStake ProofOfWork Steem ProofOfStake Monero Dash LiteCoin Mine Ripple Mine Waves';
+
+let findShort = s => 
+    // split string into array of words
+    s.split(' ')
+        // create an array containing the lengths of each word
+        .map(word => word.length)
+            // sort the lengths from least to greatest and get first
+            .sort((a, b) => a - b)[0];
+
+findShort(str); // 4
