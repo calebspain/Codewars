28cf955fc760f49c73f92a98f95c95f8da5945b1
calebspain <crspain@uh.edu>
2019-03-31 16:25:56 -0500

    Algorithm Solution

---
+++ warn-sheep.js
@@ -0,0 +1,6 @@
+let warnTheSheep = queue => {
+    return queue.indexOf("wolf") === queue.length - 1 ?
+        "Pls go away and stop eating my sheep" :
+        "Oi! Sheep number " + (queue.length - queue.indexOf("wolf") - 1) +
+        "! You are about to be eaten by a wolf!";
+}
